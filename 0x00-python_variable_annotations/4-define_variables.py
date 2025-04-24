97bc0aeb2c40de6dd4854ffd4dc88bf49309e8b8
makau6 <makauwanyoike@gmail.com>
2025-04-24 23:13:58 +0300

    define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""4-define_variables.py
+This module defines a variable and a function to print its value.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "ALX"
