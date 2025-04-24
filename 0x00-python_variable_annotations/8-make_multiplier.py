2571509a81dc4af246f070cdf26d01dcaf5fadb1
makau6 <makauwanyoike@gmail.com>
2025-04-24 23:33:34 +0300

    make multiple

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+make multiplier
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by a given multiplier
+    """
+    def multiply(n: float) -> float:
+        return n * multiplier
+
+    return multiply
