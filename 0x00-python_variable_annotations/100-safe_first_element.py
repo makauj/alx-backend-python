8a1fbd95acaa762656d06c76349c8f750f7fc5f2
makau6 <makauwanyoike@gmail.com>
2025-04-24 23:39:15 +0300

    safe first element

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""safe first element"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any]:
+    """returns the first element of a sequence or None"""
+    if lst:
+        return lst[0]
+    return None
