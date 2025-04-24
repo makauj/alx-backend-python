116edf572b0d99c3923088f8b5df90c8fd4fe436
makau6 <makauwanyoike@gmail.com>
2025-04-24 23:05:20 +0300

    to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+to_str.py
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to a string.
+
+    Args:
+        n (float): The float to convert.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
