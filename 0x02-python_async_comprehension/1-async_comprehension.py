8e86ae71286b0664d75189f9666d6f20cb3a3aba
makau6 <makauwanyoike@gmail.com>
2025-04-29 19:32:51 +0300

    async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""async comprehension"""
+
+
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """Async comprehension"""
+    comprehend = []
+    async for i in async_generator():
+        comprehend.append(i)
+    return comprehend
