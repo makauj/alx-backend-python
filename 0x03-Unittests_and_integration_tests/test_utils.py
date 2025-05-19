5681a5594b998ce65c9713f8db8d213be73d9daa
makauj <makauwanyoike@gmail.com>
2025-05-19 07:51:32 +0000

    test utils

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+the first unit test for utils.access_nested_map
+"""
+import unittest
+from unittest import TestCase
+
+
+class TestAccessNestedMap(TestCase):
+    """
+    TestAccessNestedMap class that inherits from unittest.TestCase
+    """
+
